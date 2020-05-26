ecfbf2a7fe95a3f97f6ad46fc9610ddd0142803d
Bernadette d'Almeida <b.dalmeida@hotmail.fr>
2020-05-26 20:44:02 +0200

    mister cocktail part 1

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,39 @@
+class DosesController < ApplicationController
+  before_action :set_cocktail
+   def new
+     @dose = Dose.new
+   end
+
+   def create
+     @dose = Dose.new(dose_param)
+     @dose.cocktail = @cocktail
+     @dose.save
+     if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render 'cocktail/show'
+    end
+   end
+
+
+   def delete
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail)
+
+   end
+
+  private
+
+  def dose_param
+    params.require(:dose).permit(:ingredient, :description)
+  end
+
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+end
+
+
